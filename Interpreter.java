aeef5cac1b9b47527a496c973c4595900f10a2ae
abigail martinez <abigailmartinez@abigails-MacBook-Air.local>
2024-09-18 21:04:56 -0700

    end of chapter 7

--- Interpreter.java
+++ Interpreter.java
@@ -48,6 +48,19 @@ class Interpreter implements Expr.Visitor<Object> {
         return a.equals(b);
     }
 
+    private String stringify(Object object) {
+        if (object == null)
+            return "nil";
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+        return object.toString();
+    }
+
     @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
@@ -98,5 +111,14 @@ class Interpreter implements Expr.Visitor<Object> {
                 return isEqual(left, right);
         }
         return null;
+    }
+
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
     }
 }
