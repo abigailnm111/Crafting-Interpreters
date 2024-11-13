156a6d4de37d3e9b8c18229ea3a3404a51e1cf68
abigail martinez <abigailmartinez@abigails-MacBook-Air.local>
2024-11-12 23:13:56 -0800

    chapter 11.3.5

--- Resolver.java
+++ Resolver.java
@@ -28,6 +28,42 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         return null;
     }
 
+    // binds the function names and starts a new scope. Defines the function name before resolving the body to allow recursion. 
+    @Override
+    public Void visitFunctionStmt(Stmt.Function stmt){
+        declare(stmt.name);
+        define(stmt.name);
+        resolveFunction(stmt);
+        return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        declare(stmt.name);
+        if (stmt.initializer != null) {
+            resolve(stmt.initializer);
+        }
+        define(stmt.name);
+        return null;
+    }
+
+    //resolve to check for references to other variables
+    @Override
+    public Void visitAssignExpr(Expr.Assign expr){
+        resolve(expr.value);
+        resolveLocal(expr, expr.name);
+        return null;
+    }
+
+    @Override
+    public Void visitVariableExpr(Expr.Variable expr) {
+        if (!scopes.isEmpty() && scopes.peek().get(expr.name.lexeme) == Boolean.FALSE) {
+            Lox.error(expr.name, "Can't read local variable in it's own initializer.");
+        }
+        resolveLocal(expr, expr.name);
+        return null;
+    }
+
     // does the actual resolving for statements
     private void resolve(Stmt stmt) {
         stmt.accept(this);
@@ -38,6 +74,17 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         expr.accept(this);
     }
 
+    //creates a new scope for the body of the function and binds the params. 
+    private void resolveFunction(Stmt.Function function){
+        beginScope();
+        for(Token param: function.params){
+            declare(param);
+            define(param);
+        }
+        resolve(function.body);
+        endScope();
+    }
+
     private void beginScope() {
         scopes.push(new HashMap<String, Boolean>());
     }
@@ -46,4 +93,29 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         scopes.pop();
     }
 
+    // First step of binding. Declaring the variable in the innermost scope. 'false'
+    // indicates that it's not ready yet.
+    private void declare(Token name) {
+        if (scopes.isEmpty())
+            return;
+
+        Map<String, Boolean> scope = scopes.peek();
+        scope.put(name.lexeme, false);
+    }
+
+    // Second step of binding. Marks the variable as ready.
+    private void define(Token name) {
+        if (scopes.isEmpty())
+            return;
+        scopes.peek().put(name.lexeme, true);
+    }
+
+    private void resolveLocal(Expr expr, Token name) {
+        for (int i = scopes.size() - 1; i >= 0; i--) {
+            if (scopes.get(i).containsKey(name.lexeme)) {
+                interpreter.resolve(expr, scopes.size() - 1 - i);
+                return;
+            }
+        }
+    }
 }
